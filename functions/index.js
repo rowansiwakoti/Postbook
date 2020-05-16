81e70ac76e39b174b80814dacd34fd33030d3f75
R O W A N  S I W A K O T I <rowansiwakoti@gmail.com>
2020-05-16 18:27:54 +0545

    Set up firebase, create and read data

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello World!");
+});
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('posts')
+        .get()
+        .then(data => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push(doc.data());
+            })
+            return response.json(posts);
+        })
+        .catch(err => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((request, response) => {
+    if (request.method !== 'POST') {
+        return response.status(400).json({
+            error: 'Method not allowed'
+        });
+    }
+    const newPost = {
+        body: request.body.body,
+        userHandle: request.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    }
+
+    admin.firestore().collection('posts')
+        .add(newPost)
+        .then(doc => {
+            return response.json({
+                messsage: `document ${doc.id} created successfully!`
+            });
+        })
+        .catch(err => {
+            response.status(500).json({
+                error: 'Something went wrong'
+            });
+            console.error(err);
+        })
+});
